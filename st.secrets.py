b214ffe97dcd8e6a7930349301fcef430df572f4
unknown <jl_mahdid@esi.dz>
2023-02-14 03:11:10 +0100

    files added

---
+++ st.secrets.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('st.secrets')
+
+st.write(st.secrets['message'])
+
+# https://docs.streamlit.io/streamlit-community-cloud/get-started/deploy-an-app/connect-to-data-sources/secrets-management
